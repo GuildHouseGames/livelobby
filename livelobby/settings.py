0ff7d14158d7043a1f8885ff696beed38a7dfaea
Jack Kennedy <jackrobertkennedy@gmail.com>
2018-08-04 18:12:45 +1000

    Update base settings to allow serving static files

--- livelobby/settings.py
+++ livelobby/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
