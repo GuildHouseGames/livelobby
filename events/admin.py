a0f9b53607dc80cd6a1532537302c6112f866e12
Jack Kennedy <jackrobertkennedy@gmail.com>
2018-08-17 21:48:44 +1000

    Create event models with fields from log book

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from events.models import Event
+from events.models import Event, Participant, EventType
+
 # Register your models here.
+admin.site.register(EventType)
 admin.site.register(Event)
+admin.site.register(Participant)
--- events/models.py
+++ events/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 
+class EventType(models.Model):
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
+
 class Event(models.Model):
-    pass
+    name = models.CharField(max_length=75, default='Event')
+    time = models.DateTimeField(null=True)
+    location = models.CharField(max_length=75, default='Guild')
+    description = models.CharField(max_length=300, null=True, blank=True)
+    max_size = models.PositiveSmallIntegerField(default=4)
+    initial_size = models.PositiveSmallIntegerField(default=0)
+    type = models.ForeignKey(EventType, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Participant(models.Model):
+    name = models.CharField(max_length=75)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
