c06458c955139a6e978000bec140bb049bdc7f27
Jack Kennedy <jackrobertkennedy@gmail.com>
2018-08-18 12:33:19 +1000

    Add validation to event models

--- events/models.py
+++ events/models.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from datetime import datetime
+from pytz import timezone
 
 class EventType(models.Model):
     name = models.CharField(max_length=75)
@@ -6,14 +10,28 @@ class EventType(models.Model):
     def __str__(self):
         return self.name
 
+def future_event_time(value):
+    TIME_ZONE = getattr(settings,"TIME_ZONE", "UTC")
+    now = datetime.now(timezone(TIME_ZONE))
+    if value < now:
+        raise ValidationError("The event time must be in the future")
+
 class Event(models.Model):
     name = models.CharField(max_length=75, default='Event')
-    time = models.DateTimeField(null=True)
+    time = models.DateTimeField(null=True, validators=[future_event_time])
     location = models.CharField(max_length=75, default='Guild')
     description = models.CharField(max_length=300, null=True, blank=True)
-    max_size = models.PositiveSmallIntegerField(default=4)
     initial_size = models.PositiveSmallIntegerField(default=0)
+    max_size = models.PositiveSmallIntegerField(default=4)
     type = models.ForeignKey(EventType, on_delete=models.CASCADE, null=True)
+
+    def clean(self):
+        if self.initial_size > self.max_size:
+            raise ValidationError("The initial group size cannot be bigger than the max group size")
+
+    def save(self, *args,**kwargs):
+        self.clean()
+        super(Event, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -21,6 +39,15 @@ class Event(models.Model):
 class Participant(models.Model):
     name = models.CharField(max_length=75)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def clean(self):
+        joined_players = Participant.objects.filter(event=self.event)
+        if (joined_players.count() + 1) > self.event.max_size - self.event.initial_size:
+            raise ValidationError("The event is full")
+
+    def save(self,*args,**kwargs):
+        self.clean()
+        super(Participant, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
