e859bee020e063c73f9cbbb3b563cfd821d99114
Brian Vatcky <u5830056@anu.edu.au>
2018-08-22 02:11:09 +1000

    HTML/CSS for main events page created, basic view implemented (ListView)

--- events/views.py
+++ events/views.py
@@ -5,6 +5,10 @@ class EventListView(ListView):
     template_name = 'events/event_list.html'
     model = Event
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 class EventView(DetailView):
     template_name = 'events/event.html'
     model = Event
