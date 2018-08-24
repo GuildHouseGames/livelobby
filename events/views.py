e51c0d2f93aa4dd417156aff67273f0aa96fa019
Jack Kennedy <jackrobertkennedy@gmail.com>
2018-08-24 23:45:11 +1000

    Add form for join an event under /events/<event_id>/join

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
-from events.models import Event
-from django.views.generic import DetailView, ListView
+from events.models import Event, Participant
+from events.forms import JoinForm
+from  django.urls import reverse
+from django.views.generic import CreateView, DetailView, ListView
 
 class EventListView(ListView):
     template_name = 'events/event_list.html'
@@ -8,3 +10,14 @@ class EventListView(ListView):
 class EventView(DetailView):
     template_name = 'events/event.html'
     model = Event
+
+class JoinView(CreateView):
+    template_name = 'events/join_event.html'
+    model = Participant
+    form_class = JoinForm
+
+    def get_success_url(self):
+        return reverse('event_view', kwargs={'pk':self.kwargs['event_id']})
+
+    def get_initial(self):
+        return {'event': Event.objects.get(pk=self.kwargs['event_id'])}
