16e8e11a245860787f212fc77d62365d32f9debf
Jack Kennedy <jackrobertkennedy@gmail.com>
2018-08-30 02:43:06 +1000

    Add bootstrap to drop downs

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from events.models import Participant, Event
-from django.contrib.admin import widgets
-from datetime import datetime
 from .settings import BOOKING_TOMORROW, BOOKING_TIMES_CHOICES, MAX_SIZE_CHOICES, INITIAL_SIZE_CHOICES
 
 class JoinForm(forms.ModelForm):
@@ -16,10 +14,30 @@ class JoinForm(forms.ModelForm):
         }
 
 class CreateEventForm(forms.ModelForm):
-    time = forms.ChoiceField(choices = BOOKING_TIMES_CHOICES, initial='', widget=forms.Select(), required=True, help_text="The time this event will take place")
-    date = forms.DateField(initial=BOOKING_TOMORROW, widget=forms.SelectDateWidget(), help_text="The date your event will take place")
-    max_size = forms.ChoiceField(choices = MAX_SIZE_CHOICES, help_text="The maximum number of players for this event")
-    initial_size = forms.ChoiceField(choices = INITIAL_SIZE_CHOICES, help_text="The initial number of players for this event (Including the host, must be below the maximum number of players)")
+    time = forms.ChoiceField(
+        choices = BOOKING_TIMES_CHOICES, initial='',
+        widget=forms.Select(attrs={'class': 'form-control'}), required=True,
+        help_text="The time this event will take place"
+    )
+
+    date = forms.DateField(
+        initial=BOOKING_TOMORROW,
+        widget=forms.SelectDateWidget(attrs={'class':'form-control snps-inline-select'}),
+        help_text="The date your event will take place"
+    )
+
+    max_size = forms.ChoiceField(
+        choices = MAX_SIZE_CHOICES,
+        help_text="The maximum number of players for this event",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    initial_size = forms.ChoiceField(
+        choices = INITIAL_SIZE_CHOICES,
+        help_text="The initial number of players for this event (Including the host, must be below the maximum number of players)",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
     host = forms.CharField(label='Host Name', max_length=75, widget=forms.TextInput(attrs={'class': 'form-control',' placeholder': 'Host name...'}))
     class Meta:
         model = Event
@@ -32,7 +50,7 @@ class CreateEventForm(forms.ModelForm):
                 attrs={ 'class':'form-control',
                         'rows':'5',
                         'placeholder':'Event description...'}),
-            'type': forms.Select(attrs={'class': 'bootstrap-select'}),
+            'type': forms.Select(attrs={'class': 'form-control'}),
         }
         help_texts = {
             'name': 'To help other players find the event they\'re looking for',
