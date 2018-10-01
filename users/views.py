88ba4544641429fb4ae61cb3ce9b11353484c05e
Brian Vatcky <u5830056@anu.edu.au>
2018-10-02 01:00:44 +1000

    Draft code for changing display name on login (commented)

--- users/views.py
+++ users/views.py
@@ -5,7 +5,22 @@ from django.views import generic
 
 from users.forms import LiveLobbyUserCreationForm
 
+from allauth.socialaccount.forms import SignupForm
+
 class SignUpView(generic.CreateView):
     form_class = LiveLobbyUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/signup.html'
+
+class MyCustomSocialSignupForm(SignupForm):
+
+    def save(self):
+
+        # Ensure you call the parent classes save.
+        # .save() returns a User object.
+        user = super(MyCustomSocialSignupForm, self).save()
+
+        # Add your own processing here.
+
+        # You must return the original result.
+        return user
