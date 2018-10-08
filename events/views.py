7d87d9f67f5373298bc6a2d1275ea9ecb5c233bc
mrpresidentjk <jackrobertkennedy@gmail.com>
2018-10-08 11:42:29 +1000

    Add material ui to leave event

--- events/views.py
+++ events/views.py
@@ -106,7 +106,7 @@ class CancelView(UserPassesTestMixin, SingleObjectMixin, TemplateView):
         return HttpResponseRedirect('/events')
 
     def test_func(self):
-        if self.request.user.is_authenticated:
+        if self.request.user.is_authenticated and not self.get_object().is_cancelled:
             return self.request.user.pk == self.get_object().host.pk
         return False
 
